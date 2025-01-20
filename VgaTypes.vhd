84146d813cf04b17495b12a7a285ab0c7fb54a76
Spyros Gourgaris <77027954+Spygour@users.noreply.github.com>
2025-01-20 10:17:20 +0200

    Update VgaTypes.vhd

--- VgaTypes.vhd
+++ VgaTypes.vhd
@@ -3,16 +3,15 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 package VgaTypes is 
-    type SpriteColor_t is array (0 to 15, 0 to 15) of std_logic_vector(8 downto 0);
     type LineColor_t is array (0 to 800) of std_logic_vector(8 downto 0);
 
 
     type Sprite_t is record
         x_start : unsigned (9 downto 0);
         y_start : unsigned (9 downto 0);
-        sprite_depth : unsigned (4 downto 0);
-        sprite_width : unsigned (4 downto 0);
-        Colors : SpriteColor_t;
+        -- Here we have the type of the sprite which will be aknowledged by the memory address to get the colors
+        -- Sprites are 16x16 and are stored in SD ram
+        sprite_type : unsigned (4 downto 0);
     end record;
 
 end VgaTypes;
