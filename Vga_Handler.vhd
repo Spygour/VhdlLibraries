7d4f69b5765f70846c0041ff19dd71b8fbc592ba
Spyros Gourgaris <77027954+Spygour@users.noreply.github.com>
2025-01-27 11:26:02 +0200

    Implement locked on main function of test handler entity

--- Vga_Handler.vhd
+++ Vga_Handler.vhd
@@ -32,7 +32,7 @@ architecture rtl of Vga_Handler is
     signal Reset_Sync : std_logic := '1';
     signal Reset_Reg : std_logic := '1';
     signal BufferIndex : integer := 0;
-	 signal BufferIndex_tmp: integer := 1; --temp value of BufferIndex
+    signal BufferIndex_tmp: integer := 1; --temp value of BufferIndex
 	 
 begin
 	VgaPll:entity work.VgaPll(SYN)
@@ -60,31 +60,32 @@ begin
                  LineBuffer => LineBuffer,
                  x_axis    => x_axis,
                  y_axis    => y_axis,
-		         HsyncComplete => HsyncComplete,
-		         VsyncComplete => VsyncComplete,
-		         LineBufferIndex => BufferIndex);
+		HsyncComplete => HsyncComplete,
+		VsyncComplete => VsyncComplete,
+		LineBufferIndex => BufferIndex,
+		locked => PllLocked);
 
-    process(ColorClk, Reset_Sync)
+    process(ColorClk, Reset_Sync, PllLocked)
     begin
         if (Reset_Sync = '1') then
                 LineBuffer <= (others => (others => (others => '0')));
 				BufferIndex_tmp <= 1;
 				BufferIndex <= 0;
-        elsif rising_edge(ColorClk) then
+        elsif rising_edge(ColorClk) and locked = '1'  then
 	    -- 0xC7 = 199
             if (y_axis=x"C7") and (HsyncComplete = '0') then
                 -- 0x1FF = 255
-					 if (x_axis > x"C8" and x_axis < x"190") then
-						LineBuffer(BufferIndex_tmp) (to_integer(x_axis)) <= X"FF0000";
-					end if;
+		if (x_axis > x"C8" and x_axis < x"190") then
+			LineBuffer(BufferIndex_tmp) (to_integer(x_axis)) <= X"FF0000";
+		end if;
 	        elsif (y_axis=x"C7") and (HsyncComplete = '1') then
 		        BufferIndex_tmp <= 0;
 		        BufferIndex <= 1;
             elsif ( y_axis=x"C8" and HsyncComplete = '0') then
-		        LineBuffer(BufferIndex_tmp) (to_integer(x_axis)) <= X"00FFFF";
+		LineBuffer(BufferIndex_tmp) (to_integer(x_axis)) <= X"00FFFF";
 	        elsif ( y_axis=x"C8" and HsyncComplete = '1') then
                 BufferIndex <= 0;
-				BufferIndex_tmp <= 1;
+		BufferIndex_tmp <= 1;
             end if;
         end if;
     end process;
